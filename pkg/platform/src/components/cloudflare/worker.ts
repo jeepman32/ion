e70dbdc475f29d8dd7af5d1aea9e7a3def48642a
Jay V <air@live.ca>
2024-04-25 12:54:24 -0400

    Docs

--- pkg/platform/src/components/cloudflare/worker.ts
+++ pkg/platform/src/components/cloudflare/worker.ts
@@ -232,8 +232,7 @@ export interface WorkerArgs {
  */
 export class Worker
   extends Component
-  implements Link.Cloudflare.Linkable, Link.Linkable
-{
+  implements Link.Cloudflare.Linkable, Link.Linkable {
   private script: Output<cf.WorkerScript>;
   private workerUrl: WorkerUrl;
   private workerDomain?: cf.WorkerDomain;
@@ -386,11 +385,11 @@ export class Worker
               plainTextBindings: [
                 ...(iamCredentials
                   ? [
-                      {
-                        name: "AWS_ACCESS_KEY_ID",
-                        text: iamCredentials.id,
-                      },
-                    ]
+                    {
+                      name: "AWS_ACCESS_KEY_ID",
+                      text: iamCredentials.id,
+                    },
+                  ]
                   : []),
                 ...Object.entries(environment ?? {}).map(([key, value]) => ({
                   name: key,
@@ -401,11 +400,11 @@ export class Worker
               secretTextBindings: [
                 ...(iamCredentials
                   ? [
-                      {
-                        name: "AWS_SECRET_ACCESS_KEY",
-                        text: iamCredentials.secret,
-                      },
-                    ]
+                    {
+                      name: "AWS_SECRET_ACCESS_KEY",
+                      text: iamCredentials.secret,
+                    },
+                  ]
                   : []),
                 ...(bindings.secretTextBindings || []),
               ],
