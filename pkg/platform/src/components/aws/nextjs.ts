c71c86b08d2cab2c6201820ff530553aac3b9622
Frank <frank@sst.dev>
2024-04-27 18:23:36 -0400

    nextjs: vpc

--- pkg/platform/src/components/aws/nextjs.ts
+++ pkg/platform/src/components/aws/nextjs.ts
@@ -357,6 +357,22 @@ export interface NextjsArgs extends SsrSiteArgs {
      */
     staticEtag?: boolean;
   };
+  /**
+   * Configure the [server function](#nodes-server) in your Next.js app to connect
+   * to private subnets in a virtual private cloud or VPC. This allows your app to
+   * access private resources.
+   *
+   * @example
+   * ```js
+   * {
+   *   vpc: {
+   *     securityGroups: ["sg-0399348378a4c256c"],
+   *     subnets: ["subnet-0b6a2b73896dc8c4c", "subnet-021389ebee680c2f0"]
+   *   }
+   * }
+   * ```
+   */
+  vpc?: SsrSiteArgs["vpc"];
 }
 
 /**
